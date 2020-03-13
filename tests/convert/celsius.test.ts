0d4d64d252e489b96ffd660334bf9bd77fc9bb57
Olakunle Boye <kunlexzy@gmail.com>
2020-03-13 01:41:53 +0100

    improvement(convert): update celsius test to return number

--- tests/convert/celsius.test.ts
+++ tests/convert/celsius.test.ts
@@ -1,7 +1,7 @@
 import converToF from '../../src/convert/celsius';
 
 test('it should return a number', () => {
-  expect(converToF(32)).toBe(89.6);
+  expect(converToF(32)).toEqual(expect.any(Number));
 });
 
 test('it should return -22', () => {
@@ -10,4 +10,16 @@ test('it should return -22', () => {
 
 test('it should return -10', () => {
   expect(converToF(-10)).toEqual(14);
+});
+
+test('it should return 32', () => {
+  expect(converToF(0)).toEqual(32);
+});
+
+test('it should return 68', () => {
+  expect(converToF(20)).toEqual(68);
+});
+
+test('it should return 86', () => {
+  expect(converToF(30)).toEqual(86);
 });
